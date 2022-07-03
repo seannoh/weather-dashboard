a8a74f1d3f224468c18e7264b3c2a1bce41cb77f
Sean Oh <seanoh@ucsb.edu>
2022-07-03 13:56:21 -0700

    implement search for city and psuedocode for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $(function() {
   var currLong;
   var citiesList = [];
 
-  // Function Definitions
+  // Function Definitions --------------------------------------------------
 
     // load storage
     function loadStorage() {
@@ -21,35 +21,152 @@ $(function() {
     }
 
 
-    // display weather data
-      // if successful, add city to list
-      // update local storage
-      // update previous searches display
-      // render html
+    // Display current weather data
+    function displayWeatherData() {
+      var iconURL = "https://openweathermap.org/img/wn/";
+      var temperature;
+      var humidity;
+      var windSpeed;
+      var UVIndex;
 
+      $.ajax({
+        url: "https://api.openweathermap.org/data/3.0/onecall?lat=" + currLat + "&lon=" + currLong + "&exclude=minutely,hourly,daily,alerts&appid=af9bef06aad138cfc1edc2c4a812ee06",
+        method: "GET"
+      })
+      .fail(function( jqXHR) {
+        console.log( "Request failed: " + jqXHR.responseJSON.message);
+      })
+      .done(function(response) {
+      
+        /* add city to list */
+
+        /* update local storage */
+
+        /* display previous searches*/
+
+
+
+        temperature = response.current.temp;
+        humidity = response.current.humidity;
+        windSpeed = response.current.wind_speed;
+        UVIndex = response.current.uvi;
+        iconURL += response.current.weather[0].icon;
+        console.log(response);
+
+
+        /* Clear section */
+        /* Construct heading */
+          // add weather icon
+        /* Construct temperature*/
+        /* Construct humidity */
+        /* Construct windspeed */
+        /* Construct UV index */
+
+        /* Call to display forecast */
+
+
+      });
+    }
+
+    function displayForecast() {
+      /* Clear section */
+
+      $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + currLat + "&lon=" + currLong + "&units=imperial&appid=af9bef06aad138cfc1edc2c4a812ee06",
+        method: "GET"
+      })
+      .fail(function( jqXHR) {
+        console.log( "Request failed: " + jqXHR.responseJSON.message);
+      })
+      .done(function() {
+
+        /* Construct title */
+
+        /* Iteratively create forecast cards */
+
+
+
+
+      });
+    }
 
     // display previous searches
+    function displayPreviousSearches() {
+      //stub
+
+      /* Clear section */
+
+      /* Iteratively create button elements */
+
+    }
 
     // add search to list of previous searches
+    function addToPreviousSearches(city) {
+      //stub
+
+      /* Check if it's already in the list */
+        /* Add to list with title casing */
+    }
 
     
     // search for city upon submit
       // display weather data
+    function searchCity(e) {
+      e.preventDefault();
+      var cityArr = searchInputEl.val().split(", ");
+      var cityName = cityArr[0];
+      var countryCode = cityArr[2] || "";
+      var url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&namePrefix=" + cityName + "&sort=-population&types=CITY";
+      url += (countryCode) ? "&countryIds=" + countryCode : "";
+      $.ajax({
+          "url": url,
+          "method": "GET",
+          "headers": {
+            "X-RapidAPI-Key": "1fe342bc6amsh07f8aebea06c74bp1eff28jsn0000b1fbcb43",
+            "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+          }
+      })
+      .fail(function( jqXHR) {
+        console.log( "Request failed: " + jqXHR.responseJSON.message);
+      })
+      .done(function(response) {
+        currLat = response.data[0].latitude.toPrecision(2);
+        currLong = response.data[0].longitude.toPrecision(2);
+        currCity = response.data[0].city;
+        console.log(response);
+        displayWeatherData();
+      });
+    }
 
     // handle previous search clicks
+    function handlePreviousSearchBtns() {
+      //stub
+
       // display weather for previous search
+
+        /* set currCity */
+        /* call displayWeatherData */
 
       // remove search from list of previous searches
 
+        /* remove element from DOM */
+        /* search for and remove corresponding element in cities list */
+        /* update previousCities */
 
-  // Function Calls
+    }
+      
+
+  // Function Calls ----------------------------------------------------------
+    loadStorage();
 
 
-  // Event Listeners
+  // Event Listeners ---------------------------------------------------------
 
     // event listener for search button - search for city
+    searchBtnEl.on("click",searchCity);
 
     // event listener for previous searches - handle previous search clicks
+    previousSearchesEl.on("click",handlePreviousSearchBtns);
 
     // autocomplete for search input
     searchInputEl.autocomplete({
@@ -63,17 +180,22 @@ $(function() {
             "X-RapidAPI-Key": "1fe342bc6amsh07f8aebea06c74bp1eff28jsn0000b1fbcb43",
             "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
           },
-          success: function( data ) {
+          
+        })
+        .fail(function( jqXHR) {
+          console.log( "Request failed: " + jqXHR.responseJSON.message);
+        }) 
+        .done(function( data ) {
             citiesList = [];
             for(var i of data.data){
               citiesList.push(i.city + ", " + i.regionCode + ", " + i.countryCode);
             }
             response( citiesList );
-          },
-        } );
+        });
       },
       minLength: 3,
-    } )
+      delay: 1000
+    });
 
 
 });
